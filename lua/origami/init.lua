637a21fdecfda274814760e0fa465dc84de5ef1b
edluffy <edward@ncade.com>
2021-04-08 02:43:33 +0100

    Add working (barely) image viewer for Kitty terminal

--- lua/origami/init.lua
+++ lua/origami/init.lua
@@ -30,43 +30,52 @@ end
 function M.show_image(fname)
     local file = io.open(fname, "r")
     local t = file:read("*all")
-    --M.write_chunked({'a': 'T', 'f': 100, 'c': 50, 'r': 50}, t)
-    M.write_chunked({a='T', f=100}, t)
+    --M.write_chunked({a= 'T', f= 100, c= 50, r= 50}, t)
+    M.write_chunked({a='T', f='100'}, t)
     io.close(file)
 end
 
 function serialize_gr_command(cmd, payload)
-    local cmd_str = ''
-    for k, v in pairs(cmd) do
-        cmd_str = cmd_str..k..'='..v..','
-    end
-    cmd_str = string.sub(cmd_str, 0, -2) -- chop trailing comma
-    print(cmd_str)
+    local code = '\x1b_G'
 
-    ans = '\x1b_G'..cmd_str
+    if cmd then -- Lua table to string
+        for k, v in pairs(cmd) do
+            code = code..k..'='..v..','
+        end
+        code = code:sub(0, -2) -- chop trailing comma
+    end
+
     if payload then
-        ans = ans..';'..payload
+        code = code..';'..payload
     end
-    ans = ans..'\x1b\\'
 
-    return ans
+    return code..'\x1b\\'
 end
 
+-- Split data into 4096 byte chunks
 function M.write_chunked(cmd, data)
-    data = M.base64_encode(data)
     local chunk
-    while string.len(data) > 0 do
-        chunk = string.sub(data, 0, 4096)
-        data = string.sub(data, 4096)
-        if string.len(data) > 0 then cmd.m=1 else cmd.m=0 end
+    data = M.base64_encode(data)
+    local first = true
 
-        io.stdout:write(serialize_gr_command(cmd, chunk))
-        io.stdout:flush()
+    local stdout = vim.loop.new_pipe(false)
+    stdout:open(2)
+    
+    while #data > 0 do
+        chunk = data:sub(0, 4096)
+        data  = data:sub(4097, -1)
+
+        cmd.m = (#data > 0) and '1' or '0'
+	local seri = serialize_gr_command(cmd, chunk)
+	stdout:write(seri)
+
+	-- Unsure why this must be done
+	if first then stdout:write(seri) ; first = false end
         cmd = {}
     end
-    
 end
 
+M.show_image('/Users/edluffy/Documents/D4/images/imu5.png')
 
 return M
 
